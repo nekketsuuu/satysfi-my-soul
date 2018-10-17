53618ec98665b32c2f15c8366e55e66ad629e015
nekketsuuu <nekketsuuu@users.noreply.github.com>
2018-10-17 16:28:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# How to SATySFi
+
+## Prerequisite
+
+* Haskell Stack
+* Pandoc >= 2.3
+* [SatysfiFilter](https://github.com/nekketsuuu/SatysfiFilter)
+  * Run `stack install` to generate `SatysfiFilter-exe`.
+
+## Build
+
+```sh
+./build.hs
+```
+
+## License
+
+* Contents written by Markdown: [CC BY 3.0 Unported](https://creativecommons.org/licenses/by/3.0/)
+* Build script & templates: Apache 2.0
+
+See [LICENSE](https://github.com/nekketsuuu/how-to-satysfi/blob/master/LICENSE) for details.
---
+++ build.hs
@@ -0,0 +1,65 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-12.13 --install-ghc runghc
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+import qualified Data.Text as T
+import Shelly
+default (T.Text)
+
+-- Dirs
+
+md :: Shelly.FilePath
+md = "md"
+
+generated :: Shelly.FilePath
+generated = "generated"
+
+mdGenerated :: Shelly.FilePath
+mdGenerated = md </> generated
+
+docs :: Shelly.FilePath
+docs = "docs"
+
+docsTmp :: Shelly.FilePath
+docsTmp = docs </> generated </> "tmp"
+
+templates :: Shelly.FilePath
+templates = "templates"
+
+templatesDiversen :: Shelly.FilePath
+templatesDiversen = templates </> "diversen"
+
+-- Sources
+
+sourceBases :: [T.Text]
+sourceBases = ["index"]
+
+-- Main
+
+main :: IO ()
+main = shelly $ verbosely $ do
+  mkdir_p mdGenerated
+  mapM_ runPandoc sourceBases
+  rm_rf docs
+  cp_r mdGenerated docs
+  rm_rf docsTmp
+  mapM_ (\ file -> cp_r (templatesDiversen </> file) (docs </> ""))
+    ["jquery.sticky-kit.js", "menu", "script.js", "template.css"]
+
+runPandoc :: T.Text -> Sh ()
+runPandoc basename =
+  run_ "pandoc" ["-f", "markdown"
+                ,"-t", "html5"
+                ,"--metadata-file=metadata.yaml"
+                ,"--template", toT $ templatesDiversen </> "standalone.html"
+                ,"--css", toT $ templatesDiversen </> "template.css"
+                ,"--include-before-body", toTextIgnore $ templates </> "header.html"
+                ,"--filter", "SatysfiFilter-exe"
+                ,"--toc"
+                ,"--toc-depth=2"
+                ,"-o", toT $ mdGenerated </> fromText basename <.> "html"
+                ,toT $ md </> basename <.> "md"]
+  where toT = toTextIgnore
